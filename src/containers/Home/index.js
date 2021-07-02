7b1efb02621e7f961295c7e0c1c8b266473e9e12
Rajan Mali <mysteriouskiller50@gmail.com>
2021-07-02 13:00:35 +0545

    added styling for user cards

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -6,7 +6,9 @@ import { useSelector, useDispatch } from 'react-redux';
 import { getUserList } from './actions';
 
 // Components
-import Footer from '../../components/Footer/index';
+
+// Helper functions
+import { sanitisePhoneNumber } from '../../utils/helperFunctions';
 
 const Home = () => {
   const dispatch = useDispatch();
@@ -26,17 +28,37 @@ const Home = () => {
   return (
     <div id="wrapper">
       <div className="container">
-        {getUserListRequesting && <h2>Loading...</h2>}
-        {getUserListResponse?.[0] &&
-          getUserListResponse.map((user) => (
-            <div key={user.id}>
-              <p>Name: {user.name}</p>
-              <p>Username: {user.username}</p>
-              <p>Phone Number: {user.phone}</p>
-              <p>Website: {user.website}</p>
-            </div>
-          ))}
-        {!!getUserListError && <h1>Error!!</h1>}
+        <h1 style={{ color: 'white' }}>User List</h1>
+        <div className="row">
+          {getUserListRequesting && <h2>Loading...</h2>}
+          {getUserListResponse?.[0] &&
+            getUserListResponse.map((user) => (
+              <div className="col-sm-4" key={user.id}>
+                <div className="profile">
+                  <div className="image"></div>
+                  <h5>{user.name}</h5>
+                  <p className="username">@{user.username}</p>
+                  <p>
+                    Phone:{' '}
+                    <a href={`tel:${sanitisePhoneNumber(user.phone)}`}>
+                      {sanitisePhoneNumber(user.phone)}
+                    </a>
+                  </p>
+                  <p>
+                    Website:{' '}
+                    <a
+                      href={`https://${user.website}`}
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      {user.website}
+                    </a>
+                  </p>
+                </div>
+              </div>
+            ))}
+          {!!getUserListError && <h1>Error!!</h1>}
+        </div>
       </div>
     </div>
   );
--- src/utils/helperFunctions.js
+++ src/utils/helperFunctions.js
@@ -1,1 +1,4 @@
-// add common helper functions here
+// Add common helper functions here
+
+export const sanitisePhoneNumber = (number) =>
+  number.replace(/\D/g, '').slice(0, 10);
